e1adcadb905c6877d538e9a829ee9649c8318274
dariast03 <danteariastarifa@gmail.com>
2024-08-20 16:27:07 -0400

    feat: Script  to remove app_offline.htm file in site

---
+++ Scripts/Disable-AppOffline.ps1
@@ -0,0 +1,23 @@
+param (
+    [string]$destination,
+    [string]$recycleApp,
+    [string]$computerName,
+    [string]$username,
+    [string]$password
+)
+
+$msdeploy = "C:\Program Files (x86)\IIS\Microsoft Web Deploy V3\msdeploy.exe"
+$computerNameArgument = "$computerName/MsDeploy.axd?site=$recycleApp"
+
+$targetPath = "$recycleApp$destination"
+
+[System.Collections.ArrayList]$msdeployArguments = @(
+    "-verb:delete",
+    "-allowUntrusted",
+    "-dest:contentPath=`"$targetPath\app_offline.htm`",computerName=`"$computerNameArgument`",username=`"$username`",password=`"$password`",AuthType=Basic"
+)
+
+#$msdeployArguments.Add("-whatIf")
+#Write-Host "$msdeploy $($msdeployArguments -join ' ')"
+& $msdeploy @msdeployArguments
+Write-Host "El sitio $recycleApp se encuentra online."
